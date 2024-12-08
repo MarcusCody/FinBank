b3046331a21677f8ed5497a07fd13388acf2315a
marcus <haojie.chang@simpletruss.com>
2024-12-08 13:56:41 +0800

    [Features] Update the Screen title for Transaction Details

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
 import LoginScreen from '../screens/LoginScreen.tsx';
 import TransactionHistoryScreen from '../screens/TransactionHistoryScreen.tsx';
 import TransactionDetailScreen from '../screens/TransactionDetailScreen.tsx';
@@ -8,7 +8,7 @@ import TransactionDetailScreen from '../screens/TransactionDetailScreen.tsx';
 export type RootStackParamList = {
   Login: undefined;
   Transactions: undefined;
-  TransactionDetail: { transactionId: string };
+  TransactionDetail: {transactionId: string};
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -18,8 +18,15 @@ export default function AppNavigator() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Transactions" component={TransactionHistoryScreen} />
-        <Stack.Screen name="TransactionDetail" component={TransactionDetailScreen} />
+        <Stack.Screen
+          name="Transactions"
+          component={TransactionHistoryScreen}
+        />
+        <Stack.Screen
+          name="TransactionDetail"
+          component={TransactionDetailScreen}
+          options={{title: 'Transaction Details'}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
