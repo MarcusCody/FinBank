07da10fbceda82e096c091d19dce1026e5ca7df8
marcus <haojie.chang@simpletruss.com>
2024-12-08 22:07:57 +0800

    [Project Config] Added Apps logo and change project name and app name

--- ios/FinBankTests/FinBankTests.m
+++ ios/FinBankTests/FinBankTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SecureTransactionHistoryTests : XCTestCase
+@interface FinBankTests : XCTestCase
 
 @end
 
-@implementation SecureTransactionHistoryTests
+@implementation FinBankTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
